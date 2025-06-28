5423057ca75cb4702bd12d5102d005a6eff4f1ae
igeryoung <asd7415953@gmail.com>
2025-06-29 03:13:20 +0800

    feat/frontend calendar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ const Login = React.lazy(() => import('./views/pages/login/Login'))
 const Register = React.lazy(() => import('./views/pages/register/Register'))
 const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
+const HomeDashBoard = React.lazy(() => import('./views/pages/homeDashBoard/HomeDashBoard'))
 
 const App = () => {
   const { isColorModeSet, setColorMode } = useColorModes('coreui-free-react-admin-template-theme')
@@ -45,6 +46,7 @@ const App = () => {
 
           <Route path="404" element={<Page404 />} />
           <Route path="500" element={<Page500 />} />
+          <Route path="homeDashBoard" element={<HomeDashBoard />} />
 
           <Route path="*" element={<DefaultLayout />} />
         </Routes>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,12 @@ import 'core-js'
 import App from './App'
 import store from './store'
 
+import { registerLicense } from '@syncfusion/ej2-base'
+
+registerLicense(
+  'Ngo9BigBOggjHTQxAR8/V1JEaF5cXmRCeUx0Qnxbf1x1ZFRGal5UTnJXUiweQnxTdEBjX31ZcXRXQ2VeUkdzXEleYw==',
+)
+
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
